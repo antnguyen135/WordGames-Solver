886ce2483f5f692bee472781f051e9ee0090da66
anguyen22030 <88413796+anguyen22030@users.noreply.github.com>
2021-12-03 02:12:16 -0500

    Add files via upload

--- wordSearchFunctions.js
+++ wordSearchFunctions.js
@@ -1,10 +1,9 @@
 var displayFunction = (function() {
     var executed = false;
     return function() {
-        if (!executed) {
-
-            executed = true;
-            
+        var parentDiv = document.getElementById("main")
+        if (parentDiv.children.length<3){  
+            var executed = true;
             var div = document.createElement("div");
             div.style.width = "100";
             div.style.height = "100";
@@ -24,6 +23,13 @@ var displayFunction = (function() {
             }            
             document.getElementById("main").appendChild(div);
         }
+        else{
+            let d = document.getElementById("main");
+            while(d.firstChild){
+                    d.removeChild(d.firstChild);
+        }
+            displayFunction();
+            }
     };
 })
 ();
@@ -31,20 +37,27 @@ var displayFunction = (function() {
 var displayCrossWord = (function() {
     var executed = false;
     return function() {
-        if (!executed) {
-            executed = true;
-            var cat = document.createElement("div");
-            cat.style.width = "100";
-            cat.style.height = "100";
-            cat.style.background = "lightblue";
-            cat.id = "crossWordPuzzle";
-            var r = parseInt(document.getElementById("row").value);
-            var c = parseInt(document.getElementById("col").value);
-            var crossW = document.getElementById("wordSearchPuzz").value;
+        var parentDiv = document.getElementById("main")
+        var rawCrossW = document.getElementById("wordSearchPuzz").value;
+        const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+        var crossW ="";
+        for(var x = 0; x < rawCrossW.length; x++){
+            if(alphabet.includes(rawCrossW[x])){
+                crossW += rawCrossW[x];
+            }
+        }
+        var r = parseInt(document.getElementById("row").value);
+        var c = parseInt(document.getElementById("col").value);
+        var cat = document.createElement("div");
+        cat.style.width = "100";
+        cat.style.height = "100";
+        cat.style.background = "lightblue";
+        cat.id = "crossWordPuzzle";
+        if(crossW.length == r*c){
+        if (parentDiv.children.length<3 && parentDiv.children.length ==0){  
             var crossWordIter = 0;
-            cat.innerHTML = "Here is the cross word: ";
-
-            while(crossWordIter < r*c){
+            cat.innerHTML = "<br>Here is the crossword: ";
+            while(crossWordIter < r*c)
                 if(crossWordIter % c == 0){
                     cat.innerHTML += "<br>" + crossW[crossWordIter]+" ";
                     crossWordIter++;
@@ -53,8 +66,26 @@ var displayCrossWord = (function() {
                     cat.innerHTML += crossW[crossWordIter]+ " ";
                     crossWordIter++;
                 }
+            displayResult();
+            displayFunction();
+            document.getElementById("main").appendChild(cat);   
+        }
+        else{
+            let d = document.getElementById("main");
+            while(d.firstChild){
+                d.removeChild(d.firstChild);
+        }
+            displayCrossWord(); 
             }
-            document.getElementById("main").appendChild(cat);
+        }
+        else{
+            let d = document.getElementById("main");
+            while(d.firstChild){
+                d.removeChild(d.firstChild);
+            }
+            cat.style.background = "#FAEBD7"
+            cat.innerHTML = '<h2 style = "color:#FF6666"> Please make sure the entered word search fits the dimensions entered and is all letters</h2>';
+            document.getElementById("main").appendChild(cat);    
         }
     };
 })
@@ -62,18 +93,58 @@ var displayCrossWord = (function() {
 
 var displayResult = (function() {
     var executed = false;
-    return function() {
-        if (!executed) {
-
-            executed = true;
-            
+    return function(){
+        var parentDiv = document.getElementById("main")
+        if (parentDiv.children.length<3) {           
             var result = document.createElement("div");
             result.style.width = "100";
             result.style.height = "100";
             result.id = "wordSearch";
-            result.innerHTML = '<h2 style ="color:blue"> Results </h2>';       
+            result.innerHTML = '<h2 style ="color:blue"> <b>Results</b> </h2>';       
             document.getElementById("main").appendChild(result);
+        }
+        else{
+        let d = document.getElementById("main");
+        while(d.firstChild){
+            d.removeChild(d.firstChild);
+        }
+        displayResult();
         }
     };
 })
 ();
+
+var clearDisplay = (function(){
+    return function(){
+        let d = document.getElementById("main");
+        while(d.firstChild){
+            d.removeChild(d.firstChild);
+        }
+        document.getElementById("row").value = "";
+        document.getElementById("col").value = "";
+        document.getElementById("wordSearchPuzz").value = "";
+    }
+})();
+
+class toStore{
+    constructor(w,d,r,c){
+        this.word = w;
+        this.direction = d;
+        this.row = r;
+        this.col = c;
+    }
+    toString(){
+        let toReturn = "";
+        toReturn += this.direction + "(" + this.row + "," + this.col + "):" + this.word;
+        return toReturn;
+    }
+}
+var Dictionary =[];
+function findLargestWord(dictionary){
+
+}
+
+function getWordInPuzzle(indexRow , indexCol, dir, len, numRows, numCols){
+
+}
+
